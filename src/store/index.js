03d53299e69b87a742a454254343e9455385102c
Steven Van Dessel <steven@stevenvandessel.com>
2022-01-20 12:13:27 +0100

    Added methods for Rider data to central data store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,55 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
+import RiderService from '@/services/RiderService.js'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state: {
+    riders: [],
+    rider: {}
+  },
+  mutations: {
+    ADD_RIDER(state, rider) {
+      state.riders.push(rider)
+    },
+    SET_RIDER(state, rider) {
+      state.rider = rider
+    },
+    SET_RIDERS(state, riders) {
+      state.riders = riders
+    }
+  },
+  actions: {
+    createRider({ commit }, rider) {
+      RiderService.postEvent(rider)
+        .then(() => {
+          commit('ADD_RIDER', rider)
+        })
+        .catch(error => {
+          throw(error)
+        })
+    },
+    fetchRiders({ commit }) {
+      return RiderService.getRiders()
+        .then(response => {
+          commit('SET_RIDERS', response.data)
+        })
+        .catch(error => {
+          throw(error)
+        })
+    },
+    fetchRider({ commit, state }, id) {  
+      const existingRider = state.riders.find(rider => rider.id === id)
+      if (existingRider) {
+        commit('SET_RIDER', existingRider)
+      } else {
+        return RiderService.getRider(id)
+          .then(response => {
+            commit('SET_RIDER', response.data)
+          })
+          .catch(error => {
+            throw(error)
+          })
+      }
+    }
+  },
+  modules: {}
+})
