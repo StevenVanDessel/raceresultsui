16514ea6818e8cba4904fee3a4b31006df2d7ce2
Steven Van Dessel <steven@stevenvandessel.com>
2022-01-21 10:15:02 +0100

    Put main content in separate div with id as main

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <div id="main">
+    <router-view />
+  </div>
 </template>
 
 <style>
