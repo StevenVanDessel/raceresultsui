a36868b329752006159458032e79e96b94a4554d
Steven Van Dessel <steven@stevenvandessel.com>
2022-01-21 10:16:27 +0100

    Created RiderDetail view

---
+++ src/views/RiderDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Name: {{rider.name}}</p>
+    <p>Identifier: {{rider.identifier}}</p>
+    <p>yearOfBirth: {{rider.yearOfBirth}}</p>
+    <p>province: {{rider.province}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RiderDetail",
+  components: {
+    
+  },
+  computed: {
+    rider() {
+        console.log('retrieving rider from store: ' + this.$store.rider.id + ', ' + this.$store.rider.name)
+      return this.$store.rider
+    }
+  }
+};
+</script>
+
+<style>
+.home {
+  padding: 25px;
+}
+</style>
