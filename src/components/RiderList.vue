dd7304309c691d11ca8be24469fc9a77b47fc615
Steven Van Dessel <steven@stevenvandessel.com>
2022-01-30 18:43:44 +0100

    Added events for filtering and selection to RiderList + styling updates

--- src/components/RiderList.vue
+++ src/components/RiderList.vue
@@ -2,43 +2,83 @@
   <h1>{{ title }}</h1>
   <div id="riderOverview" v-if="error == null">
     <div v-if="riders != null && riders.length">
-      <p>Rider name contains <input type="text" v-model="riderNameFilterInput" id="riderNameFilterInput" name="riderNameFilterInput"> <input type="button" value="Filter riders" v-on:click="filterRidersOnName"></p>
+      <p>
+        Rider name contains
+        <input
+          type="text"
+          v-model="riderNameFilterInput"
+          id="riderNameFilterInput"
+          name="riderNameFilterInput"
+        />
+        <input
+          type="button"
+          value="Filter riders"
+          v-on:click="filterRidersOnName"
+        />
+      </p>
       <table id="riderTable">
-          <thead>
-              <tr><th v-if="selectionModeOn">&nbsp;</th><th>Name</th><th>Identifier</th><th>Year of Birth</th><th>Province</th></tr>
-          </thead>
-          <tbody>
-              <tr v-for="rider in riders" v-bind:key="rider.identifier">
-                <td v-if="selectionModeOn" style="text-align: center"><input type="checkbox" :id="rider.identifier" :value="rider.identifier" v-model="checkedRiderIdentifiers" /></td>
-                <td><router-link :to="{ name: 'RiderDetail', params: { id: rider.id }}" >{{ rider.name }}</router-link></td>
-                <td>{{ rider.identifier }}</td>
-                <td>{{ rider.yearOfBirth }}</td>
-                <td>{{ rider.province }}</td>
-              </tr>
-          </tbody>
+        <thead>
+          <tr>
+            <th v-if="selectionModeOn">&nbsp;</th>
+            <th>Name</th>
+            <th>Identifier</th>
+            <th>Year of Birth</th>
+            <th>Province</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="rider in riders" v-bind:key="rider.identifier">
+            <td v-if="selectionModeOn" style="text-align: center">
+              <input
+                type="checkbox"
+                :id="rider.identifier"
+                :value="rider.identifier"
+                v-model="checkedRiderIdentifiers"
+              />
+            </td>
+            <td>
+              <router-link
+                :to="{ name: 'RiderDetail', params: { id: rider.id } }"
+                >{{ rider.name }}</router-link
+              >
+            </td>
+            <td>{{ rider.identifier }}</td>
+            <td>{{ rider.yearOfBirth }}</td>
+            <td>{{ rider.province }}</td>
+          </tr>
+        </tbody>
       </table>
-      <input type="button" v-if="selectionModeOn" value="Select" style="float: right"/>
-      <p style="clear: both"> {{ checkedRiderIdentifiers }}</p>
+      <input
+        type="button"
+        v-if="selectionModeOn"
+        value="Select"
+        @click="ridersSelected"
+        style="float: right"
+      />
+      <p style="clear: both">{{ checkedRiderIdentifiers }}</p>
     </div>
     <div v-else><p>No riders to show</p></div>
   </div>
   <div v-else>
-    <h2>Oops. An issue was encountered when trying to fetch the riders. Please try again later.</h2>
+    <h2>
+      Oops. An issue was encountered when trying to fetch the riders. Please try
+      again later.
+    </h2>
     <p>The error was {{ error }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'RiderList',
+  name: "RiderList",
   data() {
     return {
       error: null,
       riderNameFilterInput: null,
       checkedRiderIdentifiers: [],
-    }
+    };
   },
-  props : {
+  props: {
     riders: {
       type: Array,
       required: true,
@@ -52,23 +92,24 @@ export default {
       type: String,
       required: false,
       default: "Rider Overview",
-    }
+    },
   },
-  created() {
-  },
-  computed: {
-  },
+  emits: ["filterRidersByNameContaining", "ridersSelected"],
+  computed: {},
   methods: {
     filterRidersOnName() {
-      this.$store.dispatch('fetchRidersByNameContaining', this.riderNameFilterInput)
-      .catch(error => {
-        this.error = error
-      })
+      this.$emit("filterRidersByNameContaining", {
+        text: this.riderNameFilterInput,
+      });
     },
-    
-  }
-}
+    ridersSelected() {
+      this.$emit("ridersSelected", {
+        ridersSelected: this.checkedRiderIdentifiers,
+      });
+    },
+  },
+  created() {},
+};
 </script>
 
-<style>
-</style>
+<style></style>
