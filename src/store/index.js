79af9b439654a0cfb3af962f06db04ea86f30d49
Steven Van Dessel <steven@stevenvandessel.com>
2022-01-30 14:09:19 +0100

    added fetchRidersByNameContaining action + styling update

--- src/store/index.js
+++ src/store/index.js
@@ -1,55 +1,64 @@
-import { createStore } from 'vuex'
-import RiderService from '@/services/RiderService.js'
+import { createStore } from "vuex";
+import RiderService from "@/services/RiderService.js";
 
 export default createStore({
   state: {
     riders: [],
-    rider: {}
+    rider: {},
   },
   mutations: {
     ADD_RIDER(state, rider) {
-      state.riders.push(rider)
+      state.riders.push(rider);
     },
     SET_RIDER(state, rider) {
-      state.rider = rider
+      state.rider = rider;
     },
     SET_RIDERS(state, riders) {
-      state.riders = riders
-    }
+      state.riders = riders;
+    },
   },
   actions: {
     createRider({ commit }, rider) {
       RiderService.postEvent(rider)
         .then(() => {
-          commit('ADD_RIDER', rider)
+          commit("ADD_RIDER", rider);
         })
-        .catch(error => {
-          throw(error)
-        })
+        .catch((error) => {
+          throw error;
+        });
     },
     fetchRiders({ commit }) {
       return RiderService.getRiders()
-        .then(response => {
-          commit('SET_RIDERS', response.data)
+        .then((response) => {
+          commit("SET_RIDERS", response.data);
         })
-        .catch(error => {
-          throw(error)
-        })
+        .catch((error) => {
+          throw error;
+        });
     },
-    fetchRider({ commit, state }, id) {  
-      const existingRider = state.riders.find(rider => rider.id === id)
+    fetchRider({ commit, state }, id) {
+      const existingRider = state.riders.find((rider) => rider.id === id);
       if (existingRider) {
-        commit('SET_RIDER', existingRider)
+        commit("SET_RIDER", existingRider);
       } else {
         return RiderService.getRider(id)
-          .then(response => {
-            commit('SET_RIDER', response.data)
+          .then((response) => {
+            commit("SET_RIDER", response.data);
           })
-          .catch(error => {
-            throw(error)
-          })
+          .catch((error) => {
+            throw error;
+          });
       }
-    }
+    },
+    fetchRidersByNameContaining({ commit }, text) {
+      return RiderService.getRidersByNameContaining(text)
+        .then((response) => {
+          commit("SET_RIDERS", response.data);
+        })
+        .catch((error) => {
+          throw error;
+        });
+    },
   },
-  modules: {}
-})
+  modules: {},
+});
